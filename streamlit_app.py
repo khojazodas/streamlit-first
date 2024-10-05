6303198592181eeb8009eaef22728c73595015b8
khojazodas <khojaevsadriddin188@gmail.com>
2024-10-05 19:13:53 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,3 +14,23 @@ with st.expander('Initial data'):
   st.write('**y**')
   y_row = df.species
   y_row
+  
+with st.sidebar:
+  st.header('Input Features')
+  island = st.selectbox('Island', ('Biscoe', 'Dream', 'Torgerson'))
+  bill_length_mm = st.slider('Bill depth (mm)', 32.1, 59.6, 43.9)
+  bill_depth_mm = st.slider('Bill depth (mm)', 13.1, 21.5, 17.2) 
+  flipper_length_mm = st.slider('flipper length (mm)', 172.0, 231.0, 201.0)
+  body_mass_g = st.slider('Body mass (g)', 2700.0, 6300.0, 4207.0)
+  gender = st.selectbox('Gender', ('male', 'female'))
+
+
+  # Create a DataFrame for the input features
+  data = {'island': island,
+          'bill_length_mm': bill_length_mm,
+          'bill_depth_mm': bill_depth_mm,
+          'flipper_length_mm': flipper_length_mm,
+          'body_mass_g': body_mass_g,
+          'sex': gender}
+  input_df = pd.DataFrame(data, index=[0])
+  input_penguins = pd.concat([input_df, X_raw], axis=0)
