cfc6489c27380d7559a37f5a4c5e790ce3915d8f
khojazodas <khojaevsadriddin188@gmail.com>
2024-10-05 18:48:14 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,14 @@ import streamlit as st
 
 st.title('Student Performance Data')
 
-st.write('Hello world!')
+with st.expander('Initial data'):
+  df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+  df
 
-st.write('How are you?')
+  st.write('**X**')
+  x_row = df.drop('species', axis=1)
+  x_row
+
+  st.write('**y**')
+  y_row = df.species
+  y_row
